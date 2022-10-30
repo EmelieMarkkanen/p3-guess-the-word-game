d8e893c0575d17d9207305092d9aa1c9c1fcc9e3
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2022-10-30 15:27:02 +0000

    Import dictionary and random, add welcome message for player

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+from dictionary import dictionary
+
+def welcomeMessage():
+    print("Welcome to Guess the word!")
+    print("The rules are simple; you have 6 lives and need to guess the secret word.")
+    print("Guess the wrong letter and loose a life, guess all the letters of the whole word correctly to win!\n")
+
