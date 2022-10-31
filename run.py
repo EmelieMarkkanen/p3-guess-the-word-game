5e92fea704474ed6207e7d9f78c7fa6d531d64ed
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2022-10-31 14:28:11 +0000

    Add game function
    Game function with while loop to let the player keep guessing, show correct letters or decrease tries if wrong. If player guess correct/wrong show corresponding  message and end game

--- run.py
+++ run.py
@@ -4,6 +4,9 @@
 import random
 from dictionary import dictionary
 
+word = "cat"
+#word = random.choice(dictionary)
+
 def welcomeMessage():
     """
     Welcome player, explain the rules and let them enter their name.
@@ -20,23 +23,51 @@ def welcomeMessage():
             print(f"Welcome to Guess the word, {player_name}!\n")
             break
     
-    print("The rules are simple; you have 6 lives and need to guess the secret word.")
-    print("Guess the wrong letter and loose a life, guess all the letters of the whole word correctly to win!\n")
+    print("The rules are simple; you have 6 tries and need to guess the secret word.")
+    print("Guess the wrong letter and loose a try, guess all the letters of the whole word correctly to win!\n")
 
-def get_word():
+#def get_word():
     """
     Generates a random word from dictionary.py in capital letters
-    """
-    word = random.choice(dictionary)
     return word.upper() 
+    """
 
+def game():
+    tries = 6
+    display = "_" * len(word)
+    game_over = False
+
+    while not game_over:
+        print(f"You have " + str(tries) + " tries left")
+        print(display)
+        guess = input("Please guess a letter: ")
+
+        i = 0
+        if guess in word:
+            while word.find(guess, i) != -1:
+                i = word.find(guess, i)
+                display = display[:i] + guess + display[i + 1:]
+                i += 1
+            print("You guessed correctly!")
+        else:
+            print("Sorry, that letter was not in the word :(")
+            tries -= 1
+
+        if word == display:
+            print(f"You guessed the correct word! The word was {word}!")
+            game_over = True
+            print("Would you like to play again? Type 'yes' to play, type 'no' to quit")
+        if tries == 0:
+            print(f"Sorry, you are out of tries and loose the game. The word was {word}.")
+            game_over = True
+            print("Would you like to play again? Type 'yes' to play, type 'no' to quit")
+        
 
 def main():
     """
-    Call functions in game
+    Call game functions
     """
     welcomeMessage()
-    get_word()
     game()
-    
+
 main()
